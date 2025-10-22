1a016dc84c708d67bca2f251de54d6d4cbcf4850
Nduka Ugochukwu <62814725+thatzerroguy@users.noreply.github.com>
2025-10-22 17:54:52 +0100

    feat: signup anf login controllers for auth have been created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ValidationPipe } from '../validation/validation.pipe';
+import { CreateUserDto, createUserSchema } from './dto/create-user.dto';
+import { LoginDto, loginSchema } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.CREATED)
+  @UsePipes(new ValidationPipe(createUserSchema))
+  @Post('signup')
+  async signup(@Body() signupDto: CreateUserDto) {
+    return await this.authService._signUp(signupDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @UsePipes(new ValidationPipe(loginSchema))
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return await this.authService._login(loginDto);
+  }
 }
